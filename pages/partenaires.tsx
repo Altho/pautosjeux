a2e45cee13a2d4bdb3623d69a2dcdedf121a9ab6
Margaux <margaux.raguenel@gmail.com>
2024-04-05 18:53:26 +0200

    update partenaires

--- pages/partenaires.tsx
+++ pages/partenaires.tsx
@@ -63,6 +63,12 @@ const Partenaires: NextPage  = () => {
       <a target="_blank" rel="noopener noreferrer"> Le Petit Forestier Lescar </a>
       </Link></p>
 
+      https://helioplans.fr/
+
+      <p><Link href="https://helioplans.fr/">
+      <a target="_blank" rel="noopener noreferrer"> Hélio Plans </a>
+      </Link></p>
+
       <p><Link href="https://www.joueclub.fr/magasin/joueclub-lons.html">
       <a target="_blank" rel="noopener noreferrer"> Joué Club Lons </a>
       </Link></p>
